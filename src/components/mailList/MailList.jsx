dd208876a0595177f3485855fd23f12d03e779ff
Malik <95544411+db-malik@users.noreply.github.com>
2022-08-30 19:44:41 +0100

    mailList  component

---
+++ src/components/mailList/MailList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import classes from "./MailList.module.css";
+
+const MailList = () => {
+	return (
+		<div className={classes.mail}>
+			<h1 className={classes.mailTitle}>Save time, save money!</h1>
+			<span className={classes.mailDesc}>
+				Sign up and we'll send the best deals to you
+			</span>
+			<div className={classes.mailInputContainer}>
+				<input type="text" placeholder="Your Email" />
+				<button>Subscribe</button>
+			</div>
+		</div>
+	);
+};
+
+export default MailList;
